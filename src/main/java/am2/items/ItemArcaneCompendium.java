beca3d85d071b1ebb23b215625565bec0946b3b8
Bogdan <bogdangtt@gmail.com>
2015-11-20 18:26:38 +0300

    Fix PR #1

--- src/main/java/am2/items/ItemArcaneCompendium.java
+++ src/main/java/am2/items/ItemArcaneCompendium.java
@@ -4,6 +4,12 @@ import am2.guis.AMGuiHelper;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
 import net.minecraft.world.World;
+import net.minecraft.client.renderer.texture.IIconRegister;
+import net.minecraft.util.IIcon;
+import net.minecraft.util.ResourceLocation;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+import am2.texture.ResourceManager;
 
 public class ItemArcaneCompendium extends ArsMagicaItem{
 
@@ -24,4 +30,9 @@ public class ItemArcaneCompendium extends ArsMagicaItem{
 		return false;
 	}
 
+	@Override
+	@SideOnly(Side.CLIENT)
+	public void registerIcons(IIconRegister par1IconRegister){
+		this.itemIcon = ResourceManager.RegisterTexture("arcanecompendium", par1IconRegister);
+	}
 }
