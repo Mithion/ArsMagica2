00a71eaf2840b643aae975bd2807555d57c1b019
LemADEC <lemadec.fr@gmail.com>
2015-12-06 03:03:09 +0100

    Fixed flowers compatibility with modded world generation
    Desert nova can now spawn on all sands, not only Vanilla.
    Tarma root can now spawn on all stones and cobblestones, not only
    Vanilla.

--- src/main/java/am2/blocks/BlockTarmaRoot.java
+++ src/main/java/am2/blocks/BlockTarmaRoot.java
@@ -1,12 +1,18 @@
 package am2.blocks;
 
+import java.util.HashSet;
+
 import net.minecraft.block.Block;
 import net.minecraft.init.Blocks;
+import net.minecraft.item.ItemStack;
 import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
 import net.minecraftforge.common.EnumPlantType;
+import net.minecraftforge.oredict.OreDictionary;
 
 public class BlockTarmaRoot extends AMFlower{
+
+	static HashSet<Block> blockStones = null;
 
 	protected BlockTarmaRoot(){
 		super();
@@ -25,7 +31,23 @@ public class BlockTarmaRoot extends AMFlower{
 
 	@Override
 	protected boolean canPlaceBlockOn(Block block){
-		return block == Blocks.stone || block == Blocks.cobblestone;
+		if (block == Blocks.stone || block == Blocks.cobblestone){
+			return true;
+		}
+		if (blockStones == null){// stone and cobblestone are defined by Forge, hence only first call will be 'true'
+			HashSet<ItemStack> itemStackStones = new HashSet<ItemStack>();
+			itemStackStones.addAll(OreDictionary.getOres("stone", false));
+			itemStackStones.addAll(OreDictionary.getOres("stoneCobble", false));
+			itemStackStones.addAll(OreDictionary.getOres("cobblestone", false));
+			blockStones = new HashSet<Block>(itemStackStones.size());
+			for (ItemStack itemStack : itemStackStones){
+				Block oreBlock = Block.getBlockFromItem(itemStack.getItem());
+				if (oreBlock != Blocks.air){
+					blockStones.add(oreBlock);
+				}
+			}
+		}
+		return blockStones != null && blockStones.contains(block);
 	}
 
 	//EoD: Tarmas should only grow in dark places
