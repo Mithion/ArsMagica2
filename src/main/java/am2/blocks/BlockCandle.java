509eeee572c41de43821e81b9f75b50df6643bac
Kolatra <t.louks88@gmail.com>
2016-02-15 08:34:03 -0700

    Fix BlockCandle

--- src/main/java/am2/blocks/BlockCandle.java
+++ src/main/java/am2/blocks/BlockCandle.java
@@ -3,14 +3,18 @@ package am2.blocks;
 import am2.blocks.tileentities.TileEntityCandle;
 import am2.items.ItemsCommonProxy;
 import net.minecraft.block.material.Material;
-import net.minecraft.client.renderer.texture.IIconRegister;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.BlockPos;
+import net.minecraft.util.EnumParticleTypes;
 import net.minecraft.util.MovingObjectPosition;
 import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 public class BlockCandle extends AMSpecialRenderBlockContainer{
@@ -22,39 +26,35 @@ public class BlockCandle extends AMSpecialRenderBlockContainer{
 		setBlockBounds(0.35f, 0f, 0.35f, 0.65f, 0.45f, 0.65f);
 	}
 
-	@Override
-	public int getLightValue(IBlockAccess world, int x, int y, int z){
-		return 14;
-	}
+    @Override
+    public int getLightValue() {
+        return 14;
+    }
 
-	@Override
+    @Override
 	public int getRenderType(){
 		return BlocksCommonProxy.blockRenderID;
 	}
 
-	@Override
-	public ArrayList<ItemStack> getDrops(World arg0, int arg1, int arg2, int arg3, int arg4, int arg5){
-		ArrayList<ItemStack> drops = new ArrayList<ItemStack>();
-		drops.add(new ItemStack(ItemsCommonProxy.wardingCandle));
-		return drops;
-	}
+    @Override
+    public List<ItemStack> getDrops(IBlockAccess world, BlockPos pos, IBlockState state, int fortune) {
+        ArrayList<ItemStack> drops = new ArrayList<ItemStack>();
+        drops.add(new ItemStack(ItemsCommonProxy.wardingCandle));
+        return drops;
+    }
 
-	@Override
-	public ItemStack getPickBlock(MovingObjectPosition target, World world, int x, int y, int z){
-		return new ItemStack(ItemsCommonProxy.wardingCandle);
-	}
+    @Override
+    public ItemStack getPickBlock(MovingObjectPosition target, World world, BlockPos pos, EntityPlayer player) {
+        return new ItemStack(ItemsCommonProxy.wardingCandle);
+    }
 
-	@Override
+    @Override
 	public TileEntity createNewTileEntity(World world, int i){
 		return new TileEntityCandle();
 	}
 
-	@Override
-	public void randomDisplayTick(World world, int x, int y, int z, Random rand){
-		world.spawnParticle("flame", x + 0.5, y + 0.65, z + 0.5, 0, 0, 0);
-	}
-
-	@Override
-	public void registerBlockIcons(IIconRegister par1IconRegister){
-	}
+    @Override
+    public void randomDisplayTick(World world, BlockPos pos, IBlockState state, Random rand) {
+        world.spawnParticle(EnumParticleTypes.FLAME, pos.getX() + 0.5, pos.getY() + 0.65, pos.getZ() + 0.5, 0, 0, 0);
+    }
 }
