ebcf7feffaa99909fbe73e26cf7bb346d459a1f9
Adrian Freund <freundTech@gmail.com>
2017-06-05 21:41:58 +0200

    Fixed Leap levels shifted by one

--- src/main/java/am2/AMEventHandler.java
+++ src/main/java/am2/AMEventHandler.java
@@ -227,7 +227,7 @@ public class AMEventHandler{
 			double zVelocity = 0;
 
 			Vec3 vec = event.entityLiving.getLookVec().normalize();
-			switch (event.entityLiving.getActivePotionEffect(BuffList.leap).getAmplifier() + 1){
+			switch (event.entityLiving.getActivePotionEffect(BuffList.leap).getAmplifier()){
 			case BuffPowerLevel.Low:
 				yVelocity = 0.4;
 				xVelocity = velocityTarget.motionX * 1.08 * Math.abs(vec.xCoord);
